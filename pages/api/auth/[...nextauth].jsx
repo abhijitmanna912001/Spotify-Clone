82b1bafef28839c41ada786ad0dce963bfd5612f
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-05-06 01:04:55 +0530

    scopes added

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -1,12 +1,26 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
+const scopes = [
+  "user-read-email",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "user-read-currently-playing",
+  "user-modify-playback-state",
+].join(",");
+
+const params = { scope: scopes };
+
+const LOGIN_URL =
+  "https://accounts.spotify.com/authorize?" +
+  new URLSearchParams(params).toString();
+
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorization: LOGIN_URL,
     }),
   ],
 };
